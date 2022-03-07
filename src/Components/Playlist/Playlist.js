a0193d35c7d3d61f6f605523dc69b9a66000ebb7
Jenessa Codes <87744322+JenessaCodes@users.noreply.github.com>
2022-03-06 22:45:48 -0800

    Add component files

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import TrackList from "../TrackList/TrackList";
+
+export class Playlist extends Component {
+  render() {
+    return (
+      <div className="Playlist">
+        <input defaultValue={"New Playlist"} />
+        <TrackList />
+        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>
+    );
+  }
+}
+
+export default Playlist;
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class TrackList extends Component {
+  render() {
+    return <div>TrackList</div>;
+  }
+}
+
+export default TrackList;
