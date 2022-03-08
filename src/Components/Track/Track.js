62c4b29245a6f63a4f7d85cce43cd9768db62df8
Jenessa Codes <87744322+JenessaCodes@users.noreply.github.com>
2022-03-07 23:31:36 -0800

    Pass state and props until TrackList file

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import "./Track.css";
 
 export class Track extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   renderAction = () => {
     if (this.props.isRemoval === true) {
       return <button className="Track-action">-</button>;
@@ -21,7 +17,7 @@ export class Track extends Component {
           <h3>track name will go here</h3>
           <p>track artist will go here | track album will go here</p>
         </div>
-        {this.renderAction()}
+        {this.renderAction}
       </div>
     );
   }
